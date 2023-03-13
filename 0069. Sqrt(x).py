4988e9545ccd56456f91878fa4b4177c860f323b
erikfriedrich <45400909+erikfriedrich@users.noreply.github.com>
2023-03-13 22:41:37 +0100

    Create 0069. Sqrt(x).py
    Statistics:
    
    Runtime: 35ms ( Beats 81.49 % )
    Memory: 13.8 MB ( Beats 93.35 % )

---
+++ 0069. Sqrt(x).py
@@ -0,0 +1,14 @@
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+# You must not use any built-in exponent function or operator.
+
+# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+
+# THIS IS MY REVENGE FOR 367 PERFECT SQUARE!!!
+
+import math
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return math.floor(sqrt(x))
