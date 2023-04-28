4ab462eb1af0f48484f55aba4d0eca2f240e5874
erikfriedrich <45400909+erikfriedrich@users.noreply.github.com>
2023-04-29 00:47:05 +0200

    Create 2570. Merge Two 2D Arrays by Summing Values.py

---
+++ 2570. Merge Two 2D Arrays by Summing Values.py
@@ -0,0 +1,31 @@
+# You are given two 2D integer arrays nums1 and nums2.
+
+# nums1[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+# nums2[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+# Each array contains unique ids and is sorted in ascending order by id.
+
+# Merge the two arrays into one array that is sorted in ascending order by id, respecting the following conditions:
+
+# Only ids that appear in at least one of the two arrays should be included in the resulting array.
+# Each id should be included only once and its value should be the sum of the values of this id in the two arrays. If the id does not exist in one of the two arrays then its value in that array is considered to be 0.
+# Return the resulting array. The returned array must be sorted in ascending order by id.
+
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        
+        # turn nums1 into a dict
+        nums = dict(nums1)
+        
+        # go trough each sublist of nums2 and add the value to the corresponding key in our dict (if the key is not there we just add it with the value)
+        for i in range(len(nums2)):
+            key = nums2[i][0]
+            value = nums2[i][1]
+            try:
+                nums[key] += value
+            except KeyError:
+                nums[key] = value
+                
+        # turn dict back into a list (and then return it sorted)
+        lst = [[key, value] for key, value in nums.items()]
+        
+        return sorted(lst)
